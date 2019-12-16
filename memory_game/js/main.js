08b1b6e0e1884220dd3002c76517e9fae8364dcd
Sara Laffin <sarala@umich.edu>
2019-12-16 14:14:21 -0700

    created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,26 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay[0]);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	// player has chosen 2 cards
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		// both cards are the same
+		alert("You found a match!");
+	}
+	else {
+		alert('Sorry, try again.')
+	}
+}
