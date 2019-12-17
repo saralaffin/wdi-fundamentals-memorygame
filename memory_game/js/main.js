5f4b300d427af5cafb0e6922d42abb3ab770e4a4
Sara Laffin <sarala@umich.edu>
2019-12-16 20:25:24 -0700

    created an object to help with game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,21 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "image/king-of-diamonds.png"
+	}];
 
 var cardsInPlay = [];
 
@@ -11,9 +28,11 @@ var checkForMatch = function() {
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].cardImage);
+	console.log("User flipped " + cards[cardId].suit);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 	if (cardsInPlay.length === 2) {
 		// player has chosen 2 cards
